84b90caa602bfadea79f3d3c595f4315a7eebd8c
shubham Agrahari <aakashagrahari26@gmail.com>
2022-06-29 09:56:25 +0530

    top view of Binary tree

---
+++ trees/topViewOfBT.java
@@ -0,0 +1,15 @@
+
+class Pair{
+    Node node ;
+    int val;
+    public Pair(Node node,int val)
+    {
+        this.node = node;
+        this.val = val ;
+    }
+}
+public class topViewOfBT {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+    }
+}
